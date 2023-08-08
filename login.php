0c4439e5d861ef845042b6b8c1bd07cff21d454e
alef6 <133623940+alef6@users.noreply.github.com>
2023-08-08 09:44:13 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+    <title>pengaduan masyarakat</title>
+    <link rel="stylesheet" type="text/css"href="style.css">
+    <meta name="viewport" content="width=device-widht,initial-scale=1.0"/>
+</head>
+<body>
+    <div id ="card">
+        <div id="card-content">
+            <div class="card-tiltle">
+                <h2>LOGIN!</h2>
+                <div class="underline-litle">
+                </div>
+            </div>
+            
+                <label for="user-email" style="padding-top: 22px">&nbsp;
+                email
+               
+</label>
+                <input id="user-email"class="from-content"type="email"
+                name="email"autocomplete="on"required/>
+            <div class="form-border"></div>
+
+            
+            <label for="user-password" style="padding-top: 22px">&nbsp;
+                password
+               
+</label>
+                <input id="user-password"class="from-content"type="password"
+                name="password"autocomplete="on"required/>
+            <div class="form-border"></div>
+
+        
+
+        <div class="link">
+            <a href="remember.php">remember</a>
+        </div>
+
+            
+</a>
+             <input id="submit-btn" type="submit" name="submit" value="LOGIN">
+             
+        </div>
+    </div>
+</body>
+</html>
